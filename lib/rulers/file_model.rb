7996fd6deed1ee086fd43bfaa365d54834670fa4
colinbankier <colinbankier@gmail.com>
2013-07-25 12:53:11 +1000

    Add update method to FileModel

--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -3,6 +3,7 @@ require "multi_json"
 module Rulers
 	module Model
 		class FileModel
+			attr_reader :id
 			@@base_dir = Dir.pwd
 
 			def self.base_dir=(dir)
@@ -13,18 +14,18 @@ module Rulers
 				@@base_dir
 			end
 
-			def self.actual_path(filename)
-				File.join(@@base_dir, filename)
-			end
-
 			def initialize(filename)
 				@filename = filename
 
 				basename = File.split(filename)[-1]
 				@id = File.basename(basename, ".json").to_i
 
-				obj = File.read(FileModel.actual_path(filename))
+				obj = File.read(self.class.relative_path(filename))
 				@hash = MultiJson.load(obj)
+			end
+
+			def self.relative_path(filename)
+				File.join(@@base_dir, filename)
 			end
 
 			def [](name)
@@ -49,14 +50,28 @@ module Rulers
 				hash["submitter"] = attrs["submitter"] || ""
 				hash["quote"] = attrs["quote"] || ""
 				hash["attribution"] = attrs["attribution"] || ""
-
-				files = Dir["db/quotes/*.json"]
+				files = Dir[relative_path("db/quotes/*.json")]
 				names = files.map { |f| f.split("/")[-1] }
 				highest = names.map { |b| b[0...-5].to_i }.max
 				highest = 0 if not highest
 				id = highest + 1
 
-				File.open(FileModel.actual_path("db/quotes/#{id}.json"), "w") do |f|
+				write_to_file(id, hash)
+				FileModel.new "db/quotes/#{id}.json"
+			end
+
+			def update(attrs)
+				@hash["submitter"] = attrs["submitter"] if attrs["submitter"] 
+				@hash["quote"] = attrs["quote"] if attrs["quote"]
+				@hash["attribution"] = attrs["attribution"] if attrs["attribution"]
+			end
+
+			def save!
+				self.class.write_to_file(@id, @hash)
+			end
+
+			def self.write_to_file(id, hash)
+				File.open(relative_path("db/quotes/#{id}.json"), "w") do |f|
 					f.write <<TEMPLATE
 {
 	"submitter": "#{hash["submitter"]}",
@@ -65,10 +80,7 @@ module Rulers
 }
 TEMPLATE
 				end
-
-				FileModel.new "db/quotes/#{id}.json"
 			end
-
 		end
 	end
 end
--- test/file_model_test.rb
+++ test/file_model_test.rb
@@ -8,23 +8,55 @@ class FileModelTest < Test::Unit::TestCase
 	end
 
 	def teardown
-		delete_db
+		# delete_db
 	end
 
 	def delete_db
-		files = Dir[File.join(BASE_DIR, "db/quotes/*.json")]
+		files = Dir[relative_path("db/quotes/*.json")]
 		files.each { |f|  File.delete(f) }
 	end
 
-	def test_should_save_and_load_new
+	def test_should_save_new
+		hash = {
+			"submitter" => "test submitter like a person",
+			"quote" => "test quote like some words",
+			"attribution" => "test attribution like who dun it"
+		}
+		FileModel.create(hash)
+		assert_has_content("db/quotes/1.json", hash)
+	end
+
+	def assert_has_content(filename, hash)
+		absolute_path = relative_path(filename)
+		assert File.exists?(absolute_path)
+		obj = File.read(absolute_path)
+		loaded = MultiJson.load(obj)
+		assert hash == loaded, "Hash not equal: #{hash}, #{loaded}"
+	end
+
+	def test_should_update_existing
 		hash = {
 			"submitter" => "test submitter",
 			"quote" => "test quote",
 			"attribution" => "test attribution"
 		}
-		FileModel.create(MultiJson.dump(hash))
-		assert File.exists?(File.join(BASE_DIR, 
-			"db/quotes/1.json"))
+		quote = FileModel.create(hash)
+		update_params = {
+			"quote" => "updated quote",
+			"attribution" => "updated attribution"
+		}
+		quote.update(update_params)
+		quote.save!
+		expected_content = {
+			"submitter" => "test submitter",
+			"quote" => "updated quote",
+			"attribution" => "updated attribution"
+		}
+		assert_has_content("db/quotes/#{quote.id}.json", expected_content)
+	end
+
+	def relative_path(filename)
+		File.join(BASE_DIR, filename)
 	end
 
 end
